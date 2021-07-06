de22f180076205957790695f99fdde097aaf2d9c
Agne-stat <71026638+Agne-stat@users.noreply.github.com>
2021-07-06 17:12:42 +0300

    importing Calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Calendar from 'react-calendar';
 
 function App() {
+  function clickDay() {
+    console.log("labas")
+  }
+  
   return (
     <div className="App">
-      
+      <Calendar onClickDay={clickDay}>
+        
+      </Calendar>
     </div>
   );
 }
